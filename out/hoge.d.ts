b0ca4ee6631b2e8c43610cb8e79200139791e1d9
RYO <ryo@RYOnoMac-Pro.local>
2023-01-26 10:18:08 +0900

    a

---
+++ out/hoge.d.ts
@@ -0,0 +1,5 @@
+export declare let hoge: (a: any, b: any) => any;
+export declare class Hoge {
+    hoge(): void;
+}
+//# sourceMappingURL=hoge.d.ts.map
---
+++ out/huga/huga.d.ts
@@ -0,0 +1,5 @@
+import { Hoge } from "../hoge";
+export declare class Huga extends Hoge {
+    hoge(): void;
+}
+//# sourceMappingURL=huga.d.ts.map
